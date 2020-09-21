1c89fe895ba160c960f63dd13b79863cd9b29f01
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-09-21 22:54:13 +0200

    Change store route to 'post'.

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', [AboutController::class, 'show'])->name('about');
+Route::get('about-us', [AboutController::class, 'show'])->name('about');
 Route::get('news', [NewsItemController::class, 'index'])->name('news');
 Route::get('news/create', [NewsItemController::class, 'create'])->name('news.create');
-Route::get('news/store', [NewsItemController::class, 'store'])->name('news.store');
+Route::post('news/store', [NewsItemController::class, 'store'])->name('news.store');
 Route::get('news/{id}', [NewsItemController::class, 'show'])->name('news.show');
